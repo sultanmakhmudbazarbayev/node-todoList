81934e9e72424fef889cebca66e1c1484b3f2ef8
sultanmakhmudbazarbayev <sultanmakhmudbazarbayev@gmail.com>
2022-06-26 21:49:13 +0600

    Add .gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -165,7 +165,13 @@ app.get("/:listName", function(req, res) {
     
 }); 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
 
-app.listen(3000, function() {
-    console.log("Server is running");
+
+app.listen(port, function() {
+    console.log("Server has started successfully!");
 });
